607b16f744d752b92d542b7ee138eda39165dc8e
Matteo Rossi <teo.red90@gmail.com>
2018-05-04 13:26:50 +0300

    Fix probability current

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -47,12 +47,12 @@ end
 
 Returns the probability current for the node ``|c ⟩``,
 defined as the expected value of the operator
-``C_c = i\\sum_{j ≠ c}(|c⟩⟨j|+|j⟩⟨c|)``, i. e.
+``C_c = i\\sum_{j ≠ c}(|c⟩⟨j|-|j⟩⟨c|)``, i. e.
 
 ```math
- \\text{Tr} [C_c ρ] = 2 ∑_{j≠c}ℑ(ρ_{cj})
+ \\text{Tr} [C_c ρ] = - 2 ∑_{j≠c}ℑ(ρ_{cj})
 ```
 """
 function probability_current(ρ::AbstractArray{Complex128,2}, c::Integer)
-    return 2 * sum(imag(ρ[c,:])) # NOTE: ρ[c,c] is real
+    return - 2 * sum(imag(ρ[c,:])) # NOTE: ρ[c,c] is real
 end
