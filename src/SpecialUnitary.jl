d0683cd206c99549a5ce4ea9d66df35917209c7c
Matteo Rossi <teo.red90@gmail.com>
2018-10-31 18:36:58 +0200

    Reduced number of allocations in igen()

--- src/SpecialUnitary.jl
+++ src/SpecialUnitary.jl
@@ -103,14 +103,13 @@ generators have to be evaluated).
 """
 function igen(f, λ, H)
     n = size(H,1)
-    #λ = sun_generators(n)
     result = spzeros(n^2-1, n^2-1)
     nimag = Int(floor(n*(n-1)/2))
     ak = zeros(n^2-1)
     ak[1:end-nimag] = real([tr(H*lambda) for lambda in λ[1:end-nimag]])
 
     for i = 1:n^2-1
-        @inbounds result[i, :] = f[i] * ak
+        @inbounds result[i, :] .= f[i] * ak
     end
     return result
 end
